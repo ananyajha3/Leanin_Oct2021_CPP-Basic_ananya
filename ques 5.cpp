35c9eac8137d5fe3dea03075356d91f7ab8d86cd
ananyajha3 <91303053+ananyajha3@users.noreply.github.com>
2021-10-11 15:38:21 +0530

    Add files via upload

---
+++ ques 5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,q,r;
+a=4;
+    b=16;
+	 q=16/4, r=16%4;
+     
+    
+    cout << "the quotient is "<<q<<endl;
+    
+    cout<< "the remainder is "<<r<< endl;
+    
+	return 0;
+}
