d54f2601078291351da7b1fdf801723840f5451a
ananyajha3 <91303053+ananyajha3@users.noreply.github.com>
2021-10-11 15:33:23 +0530

    Add files via upload

---
+++ ques 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+    	float n1,n2,n3,n4,tot ;
+		cout << "\n\n Compute the total and average of four numbers :\n";
+				
+        cout<<" Input 4 numbers (separated by space) : ";
+    
+    	cin>> n1>>n2>> n3 >> n4;
+    	tot=n1+n2+n3+n4;
+	cout<<" The total of four numbers is : "<< tot ;
+	return 0;
+}
