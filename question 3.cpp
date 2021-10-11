7479f182efbac6b17430890ca07852f893cd48bf
ananyajha3 <91303053+ananyajha3@users.noreply.github.com>
2021-10-11 16:09:38 +0530

    Add files via upload

---
+++ question 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+      int number ;
+      cout << "enter number" << endl ;
+      cin >> number ;
+      cout << ++number << endl ;
+      cout << --number << endl ;
+      cout << number --<< endl;
+      cout << number --<< endl;
+       cout << number ++<< endl;
+        cout << number ++<< endl;
+	
+	return 0;
+}
