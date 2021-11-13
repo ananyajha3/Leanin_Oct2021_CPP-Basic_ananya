8125695e59d6f748b06d4b4ec737e50deb724120
ananyajha3 <91303053+ananyajha3@users.noreply.github.com>
2021-11-13 12:10:31 +0530

    Add files via upload

---
+++ milestone-3/q5milestone3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+   int i,n,temp;
+   int arr[]={4,5,6,7,8};
+   n=5;
+  
+
+  cout<<"Reverse of the numbers in the array ";
+   for(i=n-1;i>=0;i--){
+
+    cout<<arr[i];
+   }
+
+   return 0;
+}
